bef3fe0bdd4037f0a8f827cfcd3bbf1f24850153
maselo123456 <kacperizbianski@gmail.com>
2018-05-23 11:21:01 +0200

    next

--- Project/app/src/main/java/com/example/kacper/project/MainActivity.java
+++ Project/app/src/main/java/com/example/kacper/project/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.kacper.project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button clearButton;
+    EditText editNumber;
     int a,b,wyn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
